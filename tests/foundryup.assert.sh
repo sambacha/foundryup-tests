b5cf23ab27dbb216d4d5e6f9c1c1d335fd917c64
sam bacha <sam@manifoldfinance.com>
2022-02-27 22:29:26 -0800

    fix(lint): shellfmt

--- tests/foundryup.assert.sh
+++ tests/foundryup.assert.sh
@@ -4,54 +4,53 @@ echo "libfoundryup testing harness"
 echo "version 2022.02.26"
 
 echo $BASH_VERSION
-echo date +%T 
+echo date +%T
 sleep 1
 
-
 setUp() {
-  . '../libfoundryup'
+	. '../libfoundryup'
 
-  FOUNDRY_BIN_DIR=$(mktemp -d)
-  pd=$FOUNDRY_BIN_DIR # alias to reduce character count
+	FOUNDRY_BIN_DIR=$(mktemp -d)
+	pd=$FOUNDRY_BIN_DIR # alias to reduce character count
 }
 
 tearDown() {
-  rm -rf $pd
+	rm -rf $pd
 }
 
 testUseProgramNameEmpty() {
-  use_program
-  assertEquals 2 $?
+	use_program
+	assertEquals 2 $?
 }
 
 testUseProgramVersionEmpty() {
-  use_program forge
-  assertEquals 3 $?
+	use_program forge
+	assertEquals 3 $?
 }
 
 testUseProgramProgramsDirDoesntExist() {
-  FOUNDRY_BIN_DIR='/non/existent'
-  local msg=$(
-    use_program forge 0.1.0 2>&1
-    assertEquals 1 $?
-  )
-  assertEquals "Not installed: forge 0.1.0" "$msg"
+	FOUNDRY_BIN_DIR='/non/existent'
+	local msg=$(
+		use_program forge 0.1.0 2>&1
+		assertEquals 1 $?
+	)
+	assertEquals "Not installed: forge 0.1.0" "$msg"
 }
 
 testUseProgramNotInstalled() {
-  local msg=$(
-    use_program forge 0.1.0 2>&1
-    assertEquals 1 $?
-  )
-  assertEquals "Not installed: forge 0.1.0" "$msg"
+	local msg=$(
+		use_program forge 0.1.0 2>&1
+		assertEquals 1 $?
+	)
+	assertEquals "Not installed: forge 0.1.0" "$msg"
 }
 
 testUseProgramNotInstalledInstalledOne() {
-  mkdir $pd/forge-0.1.0
+	mkdir $pd/forge-0.1.0
 
-  local msg=$(
-    use_program forge 1.5 2>&1
-    assertEquals 1 $?
-  )
-  assertEquals "Not installed: forge 1.5" "$msg"
+	local msg=$(
+		use_program forge 1.5 2>&1
+		assertEquals 1 $?
+	)
+	assertEquals "Not installed: forge 1.5" "$msg"
 }
