9c5b8d411c456212957efbf4fcaf8002e78d15bc
sam bacha <sam@manifoldfinance.com>
2023-07-17 01:38:37 -0700

    fix(version): search for .sh

--- hash-version.sh
+++ hash-version.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-HASH_VERSION=$(git rev-parse --short HEAD 2>/dev/null || find * -type f -name '*.go' -print0 | sort -z | xargs -0 sha1sum | sha1sum | sed -r 's/[^\da-f]+//g')
-echo $HASH_VERSION
+HASH_VERSION=$(git rev-parse --short HEAD 2>/dev/null || find ./* -type f -name '*.sh' -print0 | sort -z | xargs -0 sha1sum | sha1sum | sed -r 's/[^\da-f]+//g')
+echo "$HASH_VERSION"
+exut 0
